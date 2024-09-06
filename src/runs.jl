73b1699d5c35815560558acebb2fc537f5b2a3eb
Mauro Giliberti <mauro.giliberti@unifi.it>
2024-09-06 17:44:37 +0200

    remove global variables from runs

--- src/runs.jl
+++ src/runs.jl
@@ -56,8 +56,8 @@ function Run_Multiple_Strings(filename,P_list,zstar_list)
     file["zstar_list"]=zstar_list[:]
     for PP in P_list
         Threads.@threads for zz in zstar_list
-            global P=PP
-            global zstar=zz
+            P=PP
+            zstar=zz
             create_group(file, "P$(P)_z$(zstar)")
             snap_flag=false
             for i in II
